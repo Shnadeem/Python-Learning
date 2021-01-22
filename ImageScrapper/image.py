c3618b2f2431d8d5c243f289c8f9bb41afb154ae
Shahabuddin Nadeem <76179486+Shnadeem@users.noreply.github.com>
2021-01-22 15:32:14 +0300

    Update image.py

--- ImageScrapper/image.py
+++ ImageScrapper/image.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from utils.utils import apps_param, get_image_url, image_download, create_path, get_attributes
-import getopt, sys, os
+from utils.utils import image_url, apps_param, get_driver, get_image_url, image_download, create_path, get_attributes
+import sys
 
 # Get full command-line arguments and reset the default values (if passed)
 full_cmd_arguments = sys.argv
@@ -15,19 +15,23 @@ apps_param['dpath'] += apps_param['search'] + "/"
 create_path(apps_param['dpath'])
 
 # Opening the browser and downloading the image
-driver = webdriver.Firefox(executable_path = '.\geckodriver.exe')
-url_images = get_image_url(driver,apps_param['search_url'],
+start = 0
+driver = webdriver.Firefox(executable_path = './geckodriver.exe')
+driver = get_driver(driver, apps_param['search_url'],apps_param['search'])
+image_url,driver = get_image_url(driver,start,
+                           apps_param['limit'],
                            apps_param['result_locator'],
                            apps_param['image_url_locator'],
-                           apps_param['search'])
-download = image_download(driver,url_images, apps_param['limit'], apps_param['dpath'])
+                           )
+download = image_download(driver,image_url, apps_param['dpath'])
 
 print("{} images of {} downloaded successfully.".format(download,apps_param['search']))
+
 
 ## Execution
 #1. Create project in PyCharm with conda (virtual environment) python 3.8
 #2. Open terminal of this new invironment and run requirements.txt file (pip install -r requirements.txt)
-#3. download the files and folder structure as given here. Place it at home foolder of your new vertual environment
+#3. download the files and folder structure as given here. Place it at home folder of your new vertual environment
 #4. Default configurations:
 # 4.1 Seach: Eagle
 # 4.2 Limit: 10
@@ -36,4 +40,3 @@ print("{} images of {} downloaded successfully.".format(download,apps_param['sea
 #     python image.py
 # 5.2 With override parameter
 #     python image.py -s Dog -l 5  (download 5 images of Dog)
-
