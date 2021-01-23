d17bd812727b07b77f23d13223a5a3328b69cac2
Shahabuddin Nadeem <76179486+Shnadeem@users.noreply.github.com>
2021-01-23 15:16:08 +0300

    Update utils.py

--- ImageScrapper/utils/utils.py
+++ ImageScrapper/utils/utils.py
@@ -100,7 +100,7 @@ def image_download(driver,image_url,location,limit):
     i=0
     for img in image_url:
         try:
-            if i <= int(limit): # This is an extra check (even though it is not needed)
+            if i < int(limit): # This is an extra check (even though it is not needed)
                 urlretrieve(img, location + str(int(time.time())) + "_" + str(i) + ".jpg")
                 i += 1
         except:
